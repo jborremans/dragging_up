e156ddbec5bc035ef7f883bdbada0cb6f06e4a0a
joseph Borremans <joseph.borremans@gmail.com>
2012-08-10 18:26:52 -0500

    start_project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DraggingUp::Application.config.secret_token = '92014c8b0d5d43832ded4f8aca2409c6b753c39b55bbad0361af2ef730d32138a3499b817426439edf33ccfd71de87efb7f5c0afabc2937cfb33190b5454b316'
