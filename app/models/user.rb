3faecd87fe65370d129ee886e6fb66422c989307
joseph Borremans <joseph.borremans@gmail.com>
2012-08-11 20:11:17 -0500

    sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :pasword_digest
+  attr_accessible :email, :name, :pasword_digest, :password, :password_confirmation
+  
+  has_secure_password
+  
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 DraggingUp::Application.routes.draw do
+  
+  get "sessions/new"
+  post "sessions/create"
+  get "sessions/destroy"
+  
   resources :tools
 
   resources :users
 
+  
   root to: 'pages#home'
+  
   get '/help' => 'pages#help', :as => :help
   get '/contact' => 'pages#contact'
 end
