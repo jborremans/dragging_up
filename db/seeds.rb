aeab001af11b175683ae22562e7f24ff65c53d99
joseph Borremans <joseph.borremans@gmail.com>
2012-08-16 18:10:56 -0500

    db:seed dummy database completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
 if Rails.env.development?
   Tool.destroy_all
   
-  tool_names = ['hammer', 'saw', 'screw driver', 'nail gun', 'welder', 'soldering iron']
+  tool_names = ['Hammer', 'Saw', 'Cordless Drill', 'Nail Gun', 'Welder', 'Mixer', 'Jig Saw', 'Air Compressor', 'Pneumatic Staple', 'Construction Calculator', 'Bellt Sander', 'Extension Ladder', 'Step Ladder', 'Air Hose', 'Pick Axe', 'Wrenches', 'Torpedo Level', 'Speed Square', 'Messuring Equipment']
+  
+  tool_conditions = ['Excellent', 'Good', 'Average', 'Poor']
   
   50.times do |t|
     t = Tool.new
     t.tool_name = tool_names[rand(0..(tool_names.size - 1))]
     t.tool_type = %w(power hand)[rand(0..1)]
-    t.tool_price = rand(50..500)
+    t.tool_price = rand(50..950)
+    t.tool_condition = tool_conditions[rand(0..(tool_conditions.size - 1))]
+        t.tool_picture = rand(10000..100000)
     # t.string   "tool_condition"
     # t.text     "tool_description"
     # t.string   "tool_picture"
