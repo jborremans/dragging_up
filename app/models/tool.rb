72a083c4251bda87f98e35d3b8777996f8174e62
joseph Borremans <joseph.borremans@gmail.com>
2012-08-16 18:46:30 -0500

    Model Associations

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,7 @@
 class Tool < ActiveRecord::Base
-  attr_accessible :tool_condition, :tool_description, :tool_name, :tool_picture, :tool_price, :tool_type
+  attr_accessible :tool_condition, :tool_description, :tool_name, :tool_picture, :tool_price, :tool_type, :user_id, :cart_id
+  
+  belongs_to :cart
+  belongs_to :user
+  
 end
