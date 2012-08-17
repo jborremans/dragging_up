d49281c97549b28a86179040af95f6df984380be
joseph Borremans <joseph.borremans@gmail.com>
2012-08-17 12:51:24 -0500

    add branch front end team

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,4 @@
-class Tool < ActiveRecord::Base
+  class Tool < ActiveRecord::Base
   attr_accessible :tool_condition, :tool_description, :tool_name, :tool_picture, :tool_price, :tool_type, :user_id, :cart_id
   
   belongs_to :cart
