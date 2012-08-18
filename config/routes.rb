d6d768907c328e1f367278d061a54e1e56bbf3ae
joseph Borremans <joseph.borremans@gmail.com>
2012-08-18 15:22:15 -0500

    Corrections Mailer & YML lost file

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 DraggingUp::Application.routes.draw do
-  
   resources :carts
-
+  
   get "sessions/new", :as => :signin
   post "sessions/create"
-  get "sessions/destroy", :as => :signout
+  get "sessions/destroy", as: :signout
   
   resources :tools
-
   resources :users
-
   
   root to: 'pages#home'
   
-  get '/help' => 'pages#help', :as => :help
-  get '/contact' => 'pages#contact'
+  get '/help' => 'pages#help', as: :help
+  get '/contactontact' => 'pages#contact'
 end
