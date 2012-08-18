33256ef97b78bc0b3cb3aeb0b8351feca8f95ef4
joseph Borremans <joseph.borremans@gmail.com>
2012-08-17 21:39:04 -0500

    mailer set-up final pretest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        # Tell the DraggingMailer to send a welcome Email after save
+        DraggingMailer.welcome_email(@user).deliver
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
