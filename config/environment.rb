fc782ee4915152f30ceb20be7f54b6e81e6c9e29
joseph Borremans <joseph.borremans@gmail.com>
2012-08-17 22:04:57 -0500

    Mailer Revisions

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 DraggingUp::Application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'gmail.com',
-  :user_name            => 'joseph.borremans',
-  :password             => 'jborremansXXXXXX',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
+
