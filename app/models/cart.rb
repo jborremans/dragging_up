fcac920fc4399ba2c8e2b10e41b47d856c2888f7
joseph Borremans <joseph.borremans@gmail.com>
2012-08-30 15:09:31 -0500

    Total Price Method added for Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,4 +5,10 @@ class Cart < ActiveRecord::Base
   has_many   :cartools
   has_one    :order
   
+def total_price(cart_id)
+      cartools = Cartool.find_all_by_cart_id(cart_id)
+      tool_prices = cart_items.collect { |cart_item| cart_item.tool.price }
+      tool_total = tool_prices.sum
+      total_price = tool_total * 1.1
+    end  
 end
